3bcc7f2c52c3b11c864916e7b2c13699ffb6b913
Tony Sathre <tony@ghostbit.org>
2024-12-23 12:25:09 -0600

    change output

--- setup.ps1
+++ setup.ps1
@@ -5,6 +5,6 @@ $FilterFiles = Get-ChildItem -Path $SourceDirectory -Filter '*.filter'
 
 foreach ($File in $FilterFiles) {
     $TargetPath = Join-Path -Path $TargetDirectory -ChildPath $File.Name
-    'Creating symlink: {0} -> {1}' -f $File.FullName, $TargetPath
+    Write-Host ('Creating symlink: {0} -> {1}' -f $File.Name, $TargetPath) -ForegroundColor DarkBlue
     New-Item -ItemType SymbolicLink -Path $TargetPath -Target $File.FullName -Force | Out-Null
 }
